a2312c5480b03bc9dd24130fe0f623be70e6aa99
robertoiws <rreyes@connectiveit.com.mx>
2024-07-22 01:26:11 -0600

    cambios

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from "../Header/Header";
 import imagenDerecha from "../../assets/IA.png";
-import homeImage from '../../assets/home.png';
+import homeImage from "../../assets/home.png";
 
 const Hero = () => {
 
